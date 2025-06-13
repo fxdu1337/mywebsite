f874afe8a9f1e7c1f70f32cfb3b99db96b29226c
Alex <gxantico@icloud.com>
2025-06-13 21:09:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-const targetDate = new Date('2025-09-01T00:00:00').getTime();
+const targetDate1 = new Date('2025-09-01T00:00:00').getTime();
+const targetDate2 = new Date('2025-08-09T00:00:00').getTime();
 
-function updateCountdown() {
+function updateCountdown(targetDate, ids, endMessage) {
     const now = new Date().getTime();
     const distance = targetDate - now;
 
     if (distance < 0) {
-        document.querySelector('.countdown').innerHTML = "MY BDAYYY FINALLY!!!";
+        // Show end message if countdown is over
+        for (const id of Object.values(ids)) {
+            document.getElementById(id).innerText = '';
+        }
+        document.getElementById(ids.days).innerText = endMessage;
         return;
     }
 
@@ -14,11 +19,32 @@ function updateCountdown() {
     const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    document.getElementById('days').innerText = days;
-    document.getElementById('hours').innerText = hours;
-    document.getElementById('minutes').innerText = minutes;
-    document.getElementById('seconds').innerText = seconds;
+    animateUpdate(ids.days, days);
+    animateUpdate(ids.hours, hours);
+    animateUpdate(ids.minutes, minutes);
+    animateUpdate(ids.seconds, seconds);
 }
 
-setInterval(updateCountdown, 1000);
-updateCountdown();
+function animateUpdate(id, value) {
+    const el = document.getElementById(id);
+    if (el.innerText != value) {
+        el.classList.remove('animate');
+        void el.offsetWidth; // trigger reflow
+        el.innerText = value;
+        el.classList.add('animate');
+    }
+}
+
+function updateAll() {
+    updateCountdown(targetDate1, 
+        {days: 'days1', hours: 'hours1', minutes: 'minutes1', seconds: 'seconds1'}, 
+        'MY BDAYYY FINALLY!!!'
+    );
+    updateCountdown(targetDate2, 
+        {days: 'days2', hours: 'hours2', minutes: 'minutes2', seconds: 'seconds2'}, 
+        "YAYAYAY CONGRATULATIONS BABY MWAHH U DESERVE THE WORLD AND BEYOND"
+    );
+}
+
+setInterval(updateAll, 1000);
+updateAll();
